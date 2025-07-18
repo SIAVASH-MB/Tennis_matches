6f92cefab621146fff9a50026ec618927df1def3
SIAVASH-MB <126078112+SIAVASH-MB@users.noreply.github.com>
2025-07-18 10:25:57 +0000

    Add Questions folder

---
+++ Questions/question7.py
@@ -0,0 +1,8 @@
+#  What is the average number of aces per match?
+import pandas as pd
+pd.set_option('display.max_columns', None)  # Show all columns
+PeriodInfo= pd.read_csv("/workspaces/Tennis_matches/Dataset/PeriodInfo.csv",usecols=["match_id", "statistic_name"])
+Match_count=PeriodInfo["match_id"].nunique()  # Count unique matches
+Ace_count = PeriodInfo["statistic_name"].str.contains("ace", case=False, na=False).sum()  # Count aces
+Average_aces_per_match = Ace_count / Match_count if Match_count > 0 else 0  # Calculate average
+print(f"Average number of aces per match: {Average_aces_per_match}")
