9c5e53deac9c8f1e4cb78127071643046fbbaaa1
Shirvani-hub <khadijehshirvani7@gmail.com>
2025-07-23 18:47:52 +0530

    Q16incompelete

--- Questions/question16incomplete.py
+++ Questions/question16incomplete.py
@@ -1,22 +1,53 @@
-
-import pandas as pd
-import csv
-
-
-away_team = pd.read_csv('MatchAwayTeamInfo.csv')
-home_team = pd.read_csv('MatchHomeTeamInfo.csv')
+import pandas as pd 
+import numpy as np
 
 
-df1 = pd.DataFrame(away_team)
-df2 = pd.DataFrame(home_team)
-#All Players Rank ID Away = apria
-apria = df1[['player_id','current_rank']]
-aprih = df2[['player_id','current_rank']]
+ATeam = pd.read_csv('MatchAwayTeamInfo.csv')
+HTeam = pd.read_csv('MatchHomeTeamInfo.csv')
 
-all_players = pd.concat([apria,aprih],axis=0).sort_values(by=['current_rank']).dropna()
+dfATeam = pd.DataFrame(ATeam)
+dfHTeam = pd.DataFrame(HTeam)
 
-playerByRank = all_players['player_id'].unique()
+#Top ten players
+#---------------------------------------------------------------------
+aTeamRank = dfATeam[['player_id','current_rank']].dropna().sort_values(by=['current_rank']).drop_duplicates().head(33)
+hTeamRank = dfHTeam[['player_id','current_rank']].dropna().sort_values(by=['current_rank']).drop_duplicates().head(37)
+
+frames1 = [aTeamRank,hTeamRank]
+topTenPlayers = pd.concat(frames1).sort_values(by=['current_rank'])
+
+#print(topTenPlayers)
+#----------------------------------------------------------------------
+
+#match_ids
+#----------------------------------------------------------------------
+match_idsAway = dfATeam[['player_id','match_id']]
+match_idsHome = dfHTeam[['player_id','match_id']]
+
+frames2 = [match_idsAway,match_idsHome]
+match_ids = pd.concat(frames2)
+
+
+#print(match_ids)
+#---------------------------------------------------------------------
+
+#comparing player_ids with match_id for finding the games witch top ten has played
+#---------------------------------------------------------------------
+
+def get_matching_values(df1,df2,compare_column,return_colmn):
+    matched_df = df2[df2[compare_column].isin(df1[compare_column])]
+    return matched_df[return_colmn]
+
+
+matched_ids = get_matching_values(topTenPlayers,match_ids,'player_id','match_id' )
+#print(matched_ids)
+#---------------------------------------------------------------------
+
+#
+#---------------------------------------------------------------------
 
 
 
-print(all_players['current_rank'])
+
+print()
+#----------------------------------------------------------------------
