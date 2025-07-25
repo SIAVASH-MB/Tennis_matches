11348c37af5f0a796433d98f5ffd0ec54f46fcb7
yasamankhalaj <khalaj.yasaman94@gmail.com>
2025-07-25 18:41:28 +0330

    updated 3,7,8 and added 12

--- Questions/question2.py
+++
@@ -1,8 +0,0 @@
-# What is the average height of the players? 
-import pandas as pd
-Away_team=pd.read_csv("Tennis_matches/Dataset/MatchAwayTeamInfo.csv")
-Inhome_team=pd.read_csv("Tennis_matches/Dataset/MatchHomeTeamInfo.csv")
-Away_team_height=Away_team["height"].mean()
-Inhome_team_height=Inhome_team["height"].mean()
-All_Players_height=(Away_team_height + Inhome_team_height) / 2  
-print("Average height of the players is: ", All_Players_height)
